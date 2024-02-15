a4bbcc09b6b1de5cd9c23bb2a43d1898476abcf3
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-02-15 08:47:12 +0300

    list

--- company.py
+++ company.py
@@ -1,17 +1,15 @@
-# Ask the user for their salary and years of service
 salary = float(input("Enter your salary: "))
 years_of_service = int(input("Enter your years of service: "))
 
-# Calculate the bonus based on years of service
-if years_of_service >  10:
-    bonus = salary *  0.10  #  10% bonus for more than  10 years
-elif  6 <= years_of_service <=  10:
-    bonus = salary *  0.08  #  8% bonus for  6 to  10 years
-else:
-    bonus = salary *  0.05  #  5% bonus for less than  6 years
 
-# Calculate the total salary including bonus
+if years_of_service >  10:
+    bonus = salary *  0.10 
+elif  6 <= years_of_service <=  10:
+    bonus = salary *  0.08  
+else:
+    bonus = salary *  0.05  
+
+
 total_salary = salary + bonus
 
-# Print the total salary including bonus
 print(f"Your total salary including bonus is: {total_salary}")
---
+++ lists.py
@@ -0,0 +1,4 @@
+city=["nairobi","kisumu","mombasa"]
+city.insert(2,"KISUMU")
+city[1]="ruiru"
+print(city)

35deb98e4f1bb304e175f98e70118b838ff59776
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-02-19 17:59:35 +0300

    01

--- list01.py
+++ list01.py
@@ -1,5 +1,9 @@
 numbers=[1,2,3,4,5,6,7,8]
+#append number 23 in the list
 numbers.append(23)
-letters=['a','b','c','d']
-numbers.append(letters)
+
+#inserts 22 to index 8
+numbers.insert(8,22)
+#reverse the list numbers.reverse()
+numbers.sort()
 print(numbers)

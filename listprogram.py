062cf34bff2d82abe1c9ee8df2603d13d058ce7a
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-02-19 14:06:17 +0300

    listprogram

--- listprogram.py
+++ listprogram.py
@@ -1,5 +1,4 @@
 names_string = input("Enter the names for all the students separated by white space:")
 name_list = names_string.split()
-name_list = sorted(name_list)
-name_list=list(set(name_list))
+name_list = sorted(set(name_list))
 print(name_list)

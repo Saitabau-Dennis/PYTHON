b541c5943e4e38bc076de47c4792a8f5d889a57b
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-03-03 18:35:30 +0300

    payroll

--- Payroll-System.py
+++ Payroll-System.py
@@ -1,13 +1,15 @@
+#Base class
 class Employee:
     def __init__(self,emp_id,name):
         self.emp_id=emp_id
         self.name-name
     def Calculate_payroll():
         pass
+    #Derived class inherits from Base class
     def SalaryEmployee(Employee):
       def __init__ (self,emp_id,name,weekly_salary):
          super().__init__(emp_id,name,)
          self.weekly_salary=weekly_salary
-       
+       #Derived class which inherits from class SalaryEmployee
     def CommissionEmployee(SalaryEmployee):
        pass

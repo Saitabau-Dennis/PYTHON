3439c4c5fede82d1128a9afa989298c31833b869
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-03-03 21:43:52 +0300

    payroll

--- Payroll-System.py
+++ Payroll-System.py
@@ -7,16 +7,16 @@ class Employee:
     def calculate_payroll(self):
         return 0
 
-# Derived class inherits from Base class
+
 class SalaryEmployee(Employee):
     def __init__(self, emp_id, name, weekly_salary):
         super().__init__(emp_id, name)
         self.weekly_salary = weekly_salary
 
     def calculate_payroll(self):
-        return self.weekly_salary # Return the actual value, not a string
+        return self.weekly_salary 
 
-# Another Derived class that inherits from the base class
+
 class HourlyEmployee(Employee):
     def __init__(self, emp_id, name, hours_worked, hourly_rate):
         super().__init__(emp_id, name)
@@ -24,13 +24,13 @@ class HourlyEmployee(Employee):
         self.hourly_rate = hourly_rate
 
     def calculate_payroll(self):
-        return self.hours_worked * self.hourly_rate # Return the actual value, not a string
+        return self.hours_worked * self.hourly_rate 
 
-# Derived class which inherits from class SalaryEmployee
+
 class CommissionEmployee(SalaryEmployee):
     def __init__(self, emp_id, name, weekly_salary, commission_value):
         super().__init__(emp_id, name, weekly_salary)
-        self.commission_value = commission_value # Add commission_value attribute
+        self.commission_value = commission_value 
 
     def calculate_payroll(self):
-        return self.weekly_salary + self.commission_value # Correctly calculate the payroll
+        return self.weekly_salary + self.commission_value 

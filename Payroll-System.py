c59d737b9e8db11e3ca4f981ceb36452c44aeb44
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-03-03 13:41:35 +0300

    payroll

---
+++ Payroll-System.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self,emp_id,name):
+        self.emp_id=emp_id
+        self.name-name
+    def Calculate_payroll():
+       pass
+    def SalaryEmployee(Employee):
+         pass

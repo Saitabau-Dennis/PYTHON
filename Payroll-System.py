edf9890b1e19ab8c5252259ea820853c89251bb3
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-03-04 13:17:49 +0300

    fixed some bugs

--- Payroll-System.py
+++ Payroll-System.py
@@ -2,13 +2,10 @@ class Employee:
     def __init__(self, emp_id, name):
         self.emp_id = emp_id
         self.name = name
-
-   
 class SalaryEmployee(Employee):
     def __init__(self, emp_id, name, weekly_salary):
         super().__init__(emp_id, name)
         self.weekly_salary = weekly_salary
-
     def calculate_payroll(self):
         return self.weekly_salary 
 
@@ -17,16 +14,13 @@ class HourlyEmployee(Employee):
         super().__init__(emp_id, name)
         self.hours_worked = hours_worked
         self.hourly_rate = hourly_rate
-
     def calculate_payroll(self):
-       
         return  self.hours_worked * self.hourly_rate
 
 class CommissionEmployee(SalaryEmployee):
     def __init__(self, emp_id, name, weekly_salary, commission_value):
         super().__init__(emp_id, name, weekly_salary)
         self.commission_value = commission_value 
-
     def calculate_payroll(self):
         return self.weekly_salary + self.commission_value
     

e114fcd7e8c0542ad9ab9485ea2cb703d6444d4d
Saitabau <113549219+Saitabau-Dennis@users.noreply.github.com>
2024-03-09 18:03:13 +0300

    pos system

---
+++ PointOf SaleSystem.py
@@ -0,0 +1,34 @@
+class Saleitem:
+    def __init__(self,item_id,name,unit_price):
+        self.item_id=item_id
+        self.name=name
+        self.unit_price=unit_price
+class Standarditem(Saleitem):
+    def __init__(self, item_id, name, unit_price,quantity):
+        self.quantity=quantity
+        super().__init__(item_id, name, unit_price)
+    def calculate_total(self):
+        return self.quantity * self.unit_price
+class Discountitem(Standarditem):
+    def __init__(self, item_id, name, unit_price,quantity,discount_percentage):
+        self.discount_percentage=discount_percentage
+        super().__init__(item_id, name, unit_price,quantity)
+    def calculate_total(self):
+        return self.quantity * self.unit_price/self.discount_percentage
+class Serviceitem(Saleitem):
+    def __init__(self, item_id, name,unit_price,hours,hourly_rate):
+        self.hourly_rate=hourly_rate
+        self.hours=hours
+        super().__init__(item_id, name,unit_price)
+    def calculate_total(self):
+        return self.hours * self.hourly_rate
+
+standard_item=Standarditem(200,'cooking oil',300,20)
+cost=standard_item.calculate_total()
+print("THE TOTAL COST FOR STANDARD ITEM IS:",cost)
+discount_item=Discountitem(201,'cabbage',500,60,20/100)
+cost=discount_item.calculate_total()
+print("THE TOTAL COST FOR DISCOUNT ITEM IS:",cost)
+service_item=Serviceitem(203,'car',90,4,100)
+cost=service_item.calculate_total()
+print("THE TOTAL COST FOR SERVICE ITEM IS:",cost)
